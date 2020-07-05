02baff63529faf4027e83b009313e1427bf2f635
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-07-05 13:12:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,37 +1,59 @@
 def roll_call_dwarves(dwarf_names)
-  hash = Hash.new
-  dwarf_names.each_with_index { |name, index| 
-    hash[name] = index
-    order = index + 1 
-    order.to_s
-    puts "/#{order}. *#{name}/"
-}
-hash
+  hash = Hash.new 
+  
+  dwarf_names.each_with_index do |name, index|
+    
+    if index < dwarf_names.length/2 
+      hash[name] = index
+      order = index + 1 
+      puts "/#{order}. *#{name}/"
+    else 
+     break
+    end
+  end
 end
 
-def summon_captain_planet(calls)
-  calls.collect do |call|
+
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect do |call|
     call << "!"
     call.capitalize!
-  end 
-  calls
+  end
+  planeteer_calls
 end
 
-def long_planeteer_calls(calls)
-  calls.any? do |call|
-    call.length > 4 
+
+
+def long_planeteer_calls(words)
+  words.any? do |word|
+    word.length > 4
   end
 end
 
-def find_the_cheese(foods)
-  cheese_types = ["cheddar", "gouda", "camembert"]
 
-  if foods.include?(cheese_types[0]) == true ||foods.include?(cheese_types[1]) == true ||foods.include?(cheese_types[2]) == true
-      foods.find do |item|
-        item == "cheddar" || item == "gouda" || item == "camembert"
-      end
-  else 
+
+def find_the_cheese(snacks)
+  cheese_types = ["cheddar", "gouda", "camembert", "brie"]
+  
+  if snacks.include?(cheese_types[0]) == true || snacks.include?(cheese_types[1]) == true || snacks.include?(cheese_types[2]) == true || snacks.include?(cheese_types[3]) == true
+    
+    snacks.find do |food_item|
+      food_item == "cheddar" || food_item == "gouda" || food_item == "camembert" || food_item == "brie"
+    end
+  else
     return nil 
   end
-
 end
+
+
+def words_with_b(words)
+  words.select do |name|
+    name.start_with?("b")
+  end
+end
+
+words = ["trey", "bob", "gaby", "ben"]
+
+puts words_with_b(words)
+
